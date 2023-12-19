f5c69d1487d28f24f241676875576d58c8fd84e9
dipada <daniele.dipalma68@gmail.com>
2023-12-19 18:04:25 +0100

    aaaaaaaaaaaaaaaaaaaaaa

---
+++ src/main/java/com/dipada/appppp/ApppppApplication.java
@@ -0,0 +1,13 @@
+package com.dipada.appppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApppppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipada/appppp/ApppppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipada.appppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
